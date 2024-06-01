054a4608482f5c9bc3cb1e1f890505d40fa301c1
nehAshwin <nashwin2548@gmail.com>
2024-06-01 10:53:29 -0700

    Weather Data Accessible

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -10,7 +10,7 @@ import CoreLocation
 
 class WeatherManager {
     //function to access current weather from OpenWeather API
-    func getCurrentWeather(latitude: CLLocationDegrees, longitude:  CLLocationDegrees) async throws{ //learn more about async at HTTP Request with Async Await
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude:  CLLocationDegrees) async throws -> ResponseBody { //learn more about async at HTTP Request with Async Await
         
         //url is API call for current weather data using coordinate points
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("fbeb6610846a3bf57fbc40a8f06564e2")&units=metric") else { fatalError("Missing URL")}
@@ -18,5 +18,52 @@ class WeatherManager {
         let urlRequest = URLRequest(url: url)
         
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {fatalError("Error fetching weather data")}
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
     }
+}
+
+//Model of the response body we get from calling the OpenWeather API
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
 }
--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -11,10 +11,25 @@ struct ContentView: View {
     //  StateObject so app is notified every time location changes?
     @StateObject var locationManager = LocationManager()
     
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
     var body: some View {
         VStack {
             if let location = locationManager.location {
-                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+                if let weather = weather {
+                    Text("Weather data fetched")
+                }
+                else {
+                    LoadingView().task {
+                        do {
+                            weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            
+                        } catch {
+                            print("Error getting weather: \(error)")
+                        }
+                    }
+                }
             } else {
                 if locationManager.isLoading {
                     LoadingView()
